7034921301ec0575f5e22192d4b0942150799b84
Luis-Alvarezz <luisangelug10@gmail.com>
2024-08-08 09:25:15 -0600

    Agregamos firebase  y configuracion de service account

--- index.js
+++ index.js
@@ -9,13 +9,20 @@ import { initializeApp, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 
 // Archivo almacenamiento de conexion del servidor:
-// import serviceAccount from 'ruta';
+import serviceAccount from './config/firebaseServiceAccount.json';
 
 const app = express();
 const PORT = process.env.PORT || 3010; // If comparativo para puerto. 
 
+/* Configuración de Firabase para enlazamiento */
+initializeApp({
+   credential: cert(serviceAccount)
+});
 
-/* Configuración de Firabase */
+const bd = getFirestore();
+const usuariosCollection = db.collection('usuarios');
+
+app.use(bodyParser.json); 
 
 /* Operaciones CRUD */
 
